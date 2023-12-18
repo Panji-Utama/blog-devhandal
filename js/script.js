c82ec0bfc2c5a8bda48e03ef1fe122680ad43da4
FIRMAN PANJI <fpu2468@gmail.com>
2023-12-19 00:50:36 +0700

    fix: Bug fixing v2

--- js/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function () {
-  var navbar = document.getElementById("navbar");
-  if (window.scrollY > 0) {
-    navbar.classList.add("navbar-scrolled");
-  } else {
-    navbar.classList.remove("navbar-scrolled");
-  }
-});
